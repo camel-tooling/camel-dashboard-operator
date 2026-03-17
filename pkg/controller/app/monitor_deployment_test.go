cf987ae18f88df7d0e6a4d2d07d9cc59d906ba34
Pasquale Congiusti <pasquale.congiusti@gmail.com>
2026-03-17 10:49:10 +0100

    feat(ctrl): set unknown conditions when no pods
    Closes #125

--- pkg/controller/app/monitor_deployment_test.go
+++ pkg/controller/app/monitor_deployment_test.go
@@ -19,6 +19,10 @@ package app
 
 import (
 	"context"
+	"net"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/camel-tooling/camel-dashboard-operator/pkg/apis/camel/v1alpha1"
@@ -91,10 +95,12 @@ func TestMonitorActionDeploymentScaledTo0(t *testing.T) {
 	assert.Equal(t, v1alpha1.CamelAppPhasePaused, target.Status.Phase)
 	monitored := target.Status.GetCondition("Monitored")
 	assert.NotNil(t, monitored)
-	assert.Equal(t, metav1.ConditionFalse, monitored.Status)
+	assert.Equal(t, metav1.ConditionUnknown, monitored.Status)
 	assert.Equal(t, "No active Pod available", monitored.Message)
 	healthy := target.Status.GetCondition("Healthy")
-	assert.Nil(t, healthy)
+	assert.NotNil(t, healthy)
+	assert.Equal(t, metav1.ConditionUnknown, healthy.Status)
+	assert.Equal(t, "No active Pod available", healthy.Message)
 }
 
 func TestMonitorActionDeploymentNonActivePods(t *testing.T) {
@@ -150,8 +156,6 @@ func TestMonitorActionDeploymentNonActivePods(t *testing.T) {
 	assert.Equal(t, ptr.To(int32(2)), target.Status.Replicas)
 	assert.Equal(t, v1alpha1.CamelAppPhaseRunning, target.Status.Phase)
 	assert.Len(t, target.Status.Pods, 3)
-	// NOTE: we can only test not ready pods as otherwise the logic requires to access an HTTP endpoint
-	// which is difficult to mock. The unit test for this logic is done separately.
 	assert.Contains(t, target.Status.Pods, v1alpha1.PodInfo{Name: "my-pod-1", Status: "Pending"})
 	assert.Contains(t, target.Status.Pods, v1alpha1.PodInfo{Name: "my-pod-2", Status: "Pending"})
 	assert.Contains(t, target.Status.Pods, v1alpha1.PodInfo{Name: "my-pod-3", Status: "Failed"})
@@ -162,4 +166,97 @@ func TestMonitorActionDeploymentNonActivePods(t *testing.T) {
 	healthy := target.Status.GetCondition("Healthy")
 	assert.NotNil(t, healthy)
 	assert.Equal(t, metav1.ConditionFalse, healthy.Status)
+}
+
+func TestMonitorActionDeploymentPodsRunning(t *testing.T) {
+	server := mockServer()
+	defer server.Close()
+	host, portStr, err := net.SplitHostPort(strings.TrimPrefix(server.URL, "http://"))
+	require.NoError(t, err)
+
+	app := &v1alpha1.CamelApp{}
+	app.Name = "test-app"
+	app.Namespace = "default"
+	app.Annotations = map[string]string{
+		v1alpha1.AppImportedKindLabel: "Deployment",
+		v1alpha1.AppImportedNameLabel: "my-test-deploy",
+	}
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-test-deploy",
+			Namespace: "default",
+			Annotations: map[string]string{
+				// Use the mock server for testing purposes
+				v1alpha1.AppObservabilityServicesPort: portStr,
+			},
+		},
+		Spec: appsv1.DeploymentSpec{
+			Template: v1.PodTemplateSpec{Spec: v1.PodSpec{
+				Containers: []v1.Container{
+					{Image: "my-camel-image"},
+				},
+			}},
+			Selector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": "my-camel-app"}},
+			Replicas: ptr.To(int32(1)),
+		},
+		Status: appsv1.DeploymentStatus{
+			Replicas:          1,
+			AvailableReplicas: 1,
+		},
+	}
+
+	pod1 := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{Name: "my-pod-1", Namespace: "default", Labels: map[string]string{"app": "my-camel-app"}},
+		Status: v1.PodStatus{
+			Conditions: []v1.PodCondition{{Type: corev1.PodReady, Status: corev1.ConditionTrue}},
+			Phase:      corev1.PodRunning,
+			PodIP:      host,
+		},
+	}
+
+	fakeClient, err := internal.NewFakeClient([]runtime.Object{app}, deployment, pod1)
+	require.NoError(t, err)
+
+	action := &monitorAction{}
+	action.InjectClient(fakeClient)
+
+	target, err := action.Handle(context.TODO(), app)
+
+	require.NoError(t, err)
+	require.NotNil(t, target)
+	assert.Equal(t, "my-camel-image", target.Status.Image)
+	assert.Equal(t, ptr.To(int32(1)), target.Status.Replicas)
+	assert.Equal(t, v1alpha1.CamelAppPhaseRunning, target.Status.Phase)
+	assert.Len(t, target.Status.Pods, 1)
+	assert.True(t, target.Status.Pods[0].Ready)
+
+	monitored := target.Status.GetCondition("Monitored")
+	assert.NotNil(t, monitored)
+	assert.Equal(t, metav1.ConditionTrue, monitored.Status)
+	healthy := target.Status.GetCondition("Healthy")
+	assert.NotNil(t, healthy)
+	assert.Equal(t, metav1.ConditionTrue, healthy.Status)
+}
+
+func mockServer() *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case strings.Contains(r.URL.Path, "health"):
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`{"status":"UP"}`))
+
+		case strings.Contains(r.URL.Path, "metrics"):
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(`# HELP app_info Application info
+# TYPE app_info gauge
+app_info{runtime="quarkus",version="1.0.0"} 1
+`))
+
+		default:
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(`{"error":"not found"}`))
+		}
+	}))
+	return server
 }
